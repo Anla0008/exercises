25b302ac13951aee2fd1f7575580cec3e7e18f6f
Anarikke <anla0008@stud.kea.dk>
2025-02-07 15:03:05 +0100

    Real Capitalization noter

--- realCapitalization/capitalization.js
+++ realCapitalization/capitalization.js
@@ -8,6 +8,6 @@ const capitalizedName = aName[0].toUpperCase() + aName.substring(1).toLowerCase(
 
 console.log(capitalizedName);
 
-// 	aName[0].toUpperCase(): Tager det første tegn i strengen (indeks 0, altså "P") og konverterer det til stort.
-// aName.substring(1).toLowerCase(): Tager resten af strengen fra indeks 1 og frem (det er "eter") og konverterer det til små bogstaver.
+// 	aName[0].toUpperCase(): Tager det første tegn i strengen (indeks 0, altså "A") og konverterer det til stort bogstav.
+// aName.substring(1).toLowerCase(): Tager resten af strengen fra indeks 1 og frem (det er "narikke") og konverterer det til små bogstaver.
 // +: Bruges til at sammenkæde (concatenere) de to dele, altså det store første bogstav og resten i små bogstaver.
