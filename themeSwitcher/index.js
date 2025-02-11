3fb598ae2aec08d93b7409e59cbee5cf05b7b613
Anarikke <anla0008@stud.kea.dk>
2025-02-11 10:26:40 +0100

    theme switcher

--- themeSwitcher/index.js
+++ themeSwitcher/index.js
@@ -1,6 +1,7 @@
-"use strict";
-document.getElementById("theme_select").addEventListener("change"),
-  (event) => {
-    const theme = event.target.value;
-    document.body.setAttribute("theme_select"), theme;
-  };
+document.getElementById("theme_select").addEventListener("change", (event) => {
+  console.log("DAV");
+  const theme = event.target.value;
+  //  document.body.setAttribute("data-theme", theme);
+  document.body.dataset.theme = theme;
+});
+//data-theme="dark"
